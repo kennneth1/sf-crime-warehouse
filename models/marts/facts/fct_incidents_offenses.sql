02b13a2fdca1ef0d2bdd50c89ff6358c37a43a3e
Kenneth Huang <44592913+kennneth1@users.noreply.github.com>
2026-07-07 17:24:06 -0700

    add dbutils

---
+++ models/marts/facts/fct_incidents_offenses.sql
@@ -0,0 +1,28 @@
+/* TODO: Implement once fct tables grow too large
+{{ config(
+    materialized='incremental',
+    unique_key='incident_id'
+) }}
+*/
+
+--- 1 grain = 1 incident happening at a latlong (descriptive attr) ->intersection (most granular geo dim) -> neighborhood / district
+select 
+stg.incidentNumber,
+stg.latitude,
+stg.longitude,
+c.category_id
+
+from {{ ref('stg_crime_incidents') }} stg left join {{ ref('dim_category') }} c --- testing for null dimensions for now
+on stg.incidentCategory = c.incidentCategory 
+
+/* left join {{ ref('dim_date') }} d 
+left join {{ ref('dim_date') }} dd_incident on dd_incident.date_id = cast(strftime(date_trunc('day', stg.incidentDatetime), '%Y%m%d') as int)
+left join {{ ref('dim_time') }} dt_incident on dt_incident.time_id = cast(extract(hour from stg.incidentDatetime) as int)
+left join {{ ref('dim_date') }} dd_report on dd_report.date_id = cast(strftime(date_trunc('day', stg.reportDatetime), '%Y%m%d') as int)
+left join {{ ref('dim_time') }} dt_report on dt_report.time_id = dt_report.time_id = cast(extract(hour from stg.reportDatetime) as int)
+
+    on stg.intersection = g.intersection                      
+    and stg.neighborhood = g.neighborhood                      
+    and stg.district = g.district        
+
+*/
