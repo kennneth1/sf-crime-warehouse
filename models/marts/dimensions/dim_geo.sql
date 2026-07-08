7c7e2fb21792daacfe750dbd2e278ca731b513be
Kenneth Huang <44592913+kennneth1@users.noreply.github.com>
2026-07-07 19:50:03 -0700

    move lat long to dim geo, and set as PK

--- models/marts/dimensions/dim_geo.sql
+++ models/marts/dimensions/dim_geo.sql
@@ -4,14 +4,22 @@ with distinct_geo as (
     select distinct
         intersection,
         neighborhood,
-        district
+        district,
+        latitude,
+        longitude
     from {{ ref('int_latest_crime_incidents') }}
 )
 
---- fct_crime_incidents must join on intersection, neighborhood, district to obtain the proper FK without fan-out
 select
-    {{ dbt_utils.generate_surrogate_key(['intersection', 'neighborhood', 'district']) }} as geo_id,
+    {{ dbt_utils.generate_surrogate_key([
+        'latitude',
+        'longitude'
+    ]) }} as geo_id,
     intersection,
     neighborhood,
-    district
+    district,
+    latitude,
+    longitude
 from distinct_geo
+
+
