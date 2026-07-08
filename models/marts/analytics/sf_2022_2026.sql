b0a88b1ab9cc2120cf50a0c55e86916a91c06c62
Kenneth Huang <44592913+kennneth1@users.noreply.github.com>
2026-07-08 15:25:44 -0700

    add base mart to marts

--- models/marts/analytics/sf_2022_2026.sql
+++ models/marts/analytics/sf_2022_2026.sql
@@ -18,12 +18,12 @@ r.resolution
 
 
 from fct_incident_offenses f
-join dim_date id on f.incident_date_id = id.date_id
-join dim_date rd on f.report_date_id = rd.date_id
-join dim_time it on f.incident_time_id = it.time_id
-join dim_offense c on f.offense_id = c.offense_id
-join dim_geo g on f.geo_id = g.geo_id
-join dim_resolution r on f.resolution_id = r.resolution_id
+join {{ ref('dim_date') }} id on f.incident_date_id = id.date_id
+join {{ ref('dim_date') }} rd on f.report_date_id = rd.date_id
+join {{ ref('dim_time') }} it on f.incident_time_id = it.time_id
+join {{ ref('dim_offense') }} c on f.offense_id = c.offense_id
+join {{ ref('dim_geo') }} g on f.geo_id = g.geo_id
+join {{ ref('dim_resolution') }} r on f.resolution_id = r.resolution_id
 
 where id.year>=2022 and c.offense_broad != 'Other'
 order by f.incident_number, f.incident_code, rd.calendar_date
